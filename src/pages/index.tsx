e56c234c7cf183e86ce7dad9c9f02de556ae8dea
Daniël Terwiel <daniel@terwiel.io>
2024-01-04 15:01:48 +0100

    feat: @tailwind/typography

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Test Application</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="prose font-sans">
         <h1>Test Application</h1>
       </main>
     </>
